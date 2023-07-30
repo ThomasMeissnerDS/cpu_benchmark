64a0f9925e5e8dc0337262855b2270b1dbf00bb6
ThomasMeissnerDS <meissnercorporation@gmx.de>
2023-07-30 19:59:16 +0200

    Make multithreaded

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,30 @@ use std::time::Instant;
 use std::thread::available_parallelism;
 
 
+fn add_one_loop(&n_loops: &u64) -> () {
+    for _in in 0..n_loops {
+        let _ = 1 + 1;
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let num_calcs = *(&args[1].trim().parse::<u64>().unwrap());
-    let _available_cores: u32 = available_parallelism().unwrap().get()  as u32 / 2;  // get info how many threads we can use and use half of them
+    let available_cores: u64 = available_parallelism().unwrap().get()  as u64;  // get info how many threads we can use and use half of them
+    let iter_per_core: u64 = num_calcs / available_cores;
 
     let now = Instant::now();
-    let mut num_iters: u64 = 0;
-    for _i in 0..num_calcs {
-        num_iters += 1;
+    let mut results = Vec::new();
+    let mut threads = Vec::new();
+    let range: Vec<u64> = (0..available_cores).collect();
+    for _i in range {
+        threads.push(std::thread::spawn(move || {
+            add_one_loop(&iter_per_core)
+        }));
     }
+    for thread in threads {
+        results.extend(thread.join());
+    };
     let elapsed = now.elapsed();
     let calc_per_sec: f64 = (num_calcs as f64) / (elapsed.as_secs() as f64);
     println!("Total runtime: {:.2?}", elapsed);
