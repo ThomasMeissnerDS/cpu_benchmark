0c670c9e25983bc1b1a0c4eca3ead03b04602771
ThomasMeissnerDS <meissnercorporation@gmx.de>
2023-07-30 22:30:48 +0200

    Add CPU info

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,45 @@
+use std::fs;
+use std::f64;
 use std::env;
 use std::time::Instant;
 use std::thread::available_parallelism;
 
 
+fn cpu_info() -> () {
+    let cpuinfo = fs::read_to_string("/proc/cpuinfo").unwrap();
+    let mut cpu_num = 1;
+
+    for line in cpuinfo.lines() {
+        let parts: Vec<&str> = line.split(':').collect();
+        // /proc/cpuinfo is where linux stores cpu info...
+        // splitting the : so we get the record name & the value as an array
+        if parts.len() != 2 {
+            continue;
+        }
+
+        let key = parts[0].trim();
+        let value = parts[1].trim();
+
+        if key == "model name" {
+            println!("CPU {}:", cpu_num);
+            println!("\tCPU Name: {}", value);
+            cpu_num += 1;
+        } else if key == "cpu cores" {
+            let cores = value.parse::<i32>().unwrap();
+            println!("\tNumber of Cores: {}", cores);
+        } else if key == "cpu MHz" {
+            let mhz: f64 = value.parse().unwrap();
+            let ghz = mhz / 1000.0;
+            let ghz_rounded = (ghz * 100.0).round() / 100.0;
+            println!("\tClock Speed: {:.2} GHz", ghz_rounded);
+        }
+    }
+}
+
+
 fn add_one_loop(&n_loops: &u64) -> () {
     for _in in 0..n_loops {
-        let _ = 1 + 1;
+        let _ = 100.0 * 100.0;
     }
 }
 
@@ -14,8 +48,9 @@ fn main() {
     let num_calcs_arg: Option<&String> = args.get(1);
     let num_calcs: u64 = match num_calcs_arg {
         Some(num_calcs_arg) => num_calcs_arg.trim().parse::<u64>().unwrap(),
-        None => 20000000000,
+        None => 40000000000,
     };
+    cpu_info();
 
 
     let available_cores: u64 = available_parallelism().unwrap().get()  as u64;  // get info how many threads we can use and use half of them
